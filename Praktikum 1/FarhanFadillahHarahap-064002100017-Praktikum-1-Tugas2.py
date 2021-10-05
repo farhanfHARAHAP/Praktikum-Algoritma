cf68549c058eb9af9d1e56e318a0f2808430cb1d
farhanfHARAHAP <91046795+farhanfHARAHAP@users.noreply.github.com>
2021-10-05 15:35:33 +0700

    Create FarhanFadillahHarahap-064002100017-Praktikum-1-Tugas2.py

---
+++ Praktikum 1/FarhanFadillahHarahap-064002100017-Praktikum-1-Tugas2.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 20 13:33:26 2021
+
+@author: HP
+Nama: Farhan Fadillah Harahap
+NIM: 064002100017
+"""
+## Bismillah
+print ('Selamat datang di Automatic-Measuring-Area-Of-Any-Rectangle')
+print('You can call me AMAOAR!\n')
+
+p = int(input('Panjang Persegi: '))
+l = int(input('Lebar Persegi: '))
+print('Menghitung...')
+hasil = p * l
+print('\nLebar Persegi:', hasil)
