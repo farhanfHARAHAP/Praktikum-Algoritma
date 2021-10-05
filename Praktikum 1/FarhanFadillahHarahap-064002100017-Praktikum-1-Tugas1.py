a87e459639f7093a277a077463b35c5bdb26fadd
farhanfHARAHAP <91046795+farhanfHARAHAP@users.noreply.github.com>
2021-10-05 15:34:58 +0700

    Create FarhanFadillahHarahap-064002100017-Praktikum-1-Tugas1.py

---
+++ Praktikum 1/FarhanFadillahHarahap-064002100017-Praktikum-1-Tugas1.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 20 13:07:43 2021
+
+@author: HP
+Nama: Farhan Fadillah Harahap
+NIM: 064002100017
+"""
+
+print ('Hai! Salam kenal!')
+nama = input('\nSiapa nama mu kawan?\n')
+print('\nHai!', nama, ' Asal kamu darimana?')
+daerah = input ()
+sekolah = input ('Terus, kamu SMA dimana?\n')
+
+print()
+print('Wah menyenangkan bisa berkenalan dengan kamu', nama)
+print('Kapan kapan, aku mau berkunjung ke', sekolah, daerah, '!')
