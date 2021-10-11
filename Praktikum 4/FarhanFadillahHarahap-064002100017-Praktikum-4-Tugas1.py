e01d73fb3be847e97255a2ccaec32887a48a146e
farhanfHARAHAP <91046795+farhanfHARAHAP@users.noreply.github.com>
2021-10-11 13:28:48 +0700

    Create FarhanFadillahHarahap-064002100017-Praktikum-4-Tugas1.py

---
+++ Praktikum 4/FarhanFadillahHarahap-064002100017-Praktikum-4-Tugas1.py
@@ -0,0 +1,13 @@
+'''
+Nama: Farhan Fadillah Harahap
+NIM: 064002100017
+'''
+
+x = int(input('masukkan angka maks : '))
+
+while x > 0:
+    for i in range(x):
+        print(x , end='')
+    print()
+    x -= 1
+    
